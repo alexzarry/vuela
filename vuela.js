e96e0db168a9dbfb5b03f0348b5aeb5081f46c3a
alexzarry <alex.zarry@gmail.com>
2019-02-21 17:45:52 -0500

    Update vuela.js

--- vuela.js
+++ vuela.js
@@ -3,15 +3,24 @@ var app = new Vue({
   data: {
     h1: '',
     p: '',
-    heading: 'hello world',
-    paragraph: ''
+    heading: 'Hello World',
+    paragraph: '',
+    fontSize: 6,
+    lineHeight: 5
   },
   methods: {
     replaceText: function() {
         this.heading = this.h1;
         this.paragraph = this.p;
+    },
+    splitHead: function() {
+        let arr = this.heading.split('');
+        for (i = 0; i < arr.length; i++) {
+          arr[i] = '<span>' + arr[i] + '</span>';
+        }
+        arr = arr.join('');
+        document.getElementById("header").innerHTML = arr;
     }
   },
   watch: {}
 })
-console.log(app.heading);
